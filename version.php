da67a365336d7027a26371d8b6a76b8861b848b8
Jetha Chan <jetha@moodle.com>
2015-07-16 18:43:47 +0900

    Remove empty styles.css; explicitly declare dependency on moodle-atto_sketchfab. Fixes #3.

--- version.php
+++ version.php
@@ -24,8 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015040800;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015071600;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051207.02;      // Requires this Moodle version
 $plugin->component = 'filter_sketchfab'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.01';
+$plugin->dependencies = array(
+    'atto_sketchfab' => 2015040802 // atto_sketchfab v1.03 (2015040802)
+);
